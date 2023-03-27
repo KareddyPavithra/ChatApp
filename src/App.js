900a8416fbf7439ee6eaa99fb5867b2f4afcdea6
KareddyPavithra <kareddy.p@northeastern.edu>
2023-03-26 22:09:18 -0400

    Hosted the backend on ChatEngine

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { ChatEngine } from "react-chat-engine";
+
+import './App.css';
+
+const App = () => {
+    return(
+        <ChatEngine
+            height="100vh"
+            projectID="c3ba976d-76e9-484e-9822-0e562ffccde7"
+            userName="Pavithra K"
+            userPassword="Pavithra123"
+        
+        />
+    );
+}
+
+export default App;
