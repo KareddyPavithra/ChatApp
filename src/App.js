33f058fb504b2e6577507d4500ecbff225a22d54
KareddyPavithra <kareddy.p@northeastern.edu>
2023-03-28 02:01:50 -0400

    Created LoginForm

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import { ChatEngine } from "react-chat-engine";
 
+import LoginForm from "./components/LoginForm";
 import ChatFeed  from "./components/ChatFeed";
 
 import './App.css';
 
+
 const App = () => {
+
+    if(!localStorage.getItem('username')) return <LoginForm />
+
     return(
         <ChatEngine
             height="100vh"
             projectID="c3ba976d-76e9-484e-9822-0e562ffccde7"
-            userName="Pavithra K"
-            userPassword="Pavithra123"
+            userName={localStorage.getItem('username')}
+            userPassword={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+            onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
         />
     );
-}
+};
 
 export default App;
--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -7,6 +7,17 @@ const ChatFeed = (props) => {
 
     const chat = chats && chats[activeChat];
 
+    const renderReadReceipts = (message, isMyMessage) => chat.people.map((person, index) => person.last_read === message.id && (
+        <div
+          key={`read_${index}`}
+          className="read-receipt"
+          style={{
+            float: isMyMessage ? 'right' : 'left',
+            backgroundImage: person.person.avatar && `url(${person.person.avatar})`,
+          }}
+        />
+      ));
+      
     const renderMessages = () => {
         const keys = Object.keys(messages);
 
@@ -24,14 +35,14 @@ const ChatFeed = (props) => {
                             : <ReceiveMessage message={message} lastMessage={messages[lastMessageKey]} />}
                     </div>
                     <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px'}}>
-                            read-receipts
+                        {renderReadReceipts(message, isMyMessage)}
                     </div>
                 </div>
             );
         })
     }
 
-    if(!chat) return 'Loading';
+    if(!chat) return <div />;
 
     return (
         <div className="chat-feed">
